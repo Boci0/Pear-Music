79fd1c15e66d89c20511beceeb1eefd9004342af
Muhd Boci <muhdboci011@gmail.com>
2026-08-15 16:03:30 +0800

    Update in-app currentVersion constant to 1.2.4

--- app/lib/services/update_service.dart
+++ app/lib/services/update_service.dart
@@ -30,7 +30,7 @@ class UpdateInfo {
 }
 
 class UpdateService {
-  static const String currentVersion = '1.2.3';
+  static const String currentVersion = '1.2.4';
   static const String _releasesApiUrl =
       'https://api.github.com/repos/Boci0/Pear-Music/releases/latest';
 
