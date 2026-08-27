0823f2dfbe759328fa82d88825e7816c9ec166cc
Muhd Boci <muhdboci011@gmail.com>
2026-08-28 00:02:18 +0800

    chore: sanitize and parameterize set_phone_server_url script

--- tool/set_phone_server_url.ps1
+++ tool/set_phone_server_url.ps1
@@ -1,16 +1,22 @@
-# Sets the phone's signaling server URL to the PC's embedded server
-# (ws://10.84.188.119:8080), preserving the existing device id/secret.
+# Sets the phone's signaling server URL via adb.
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$ServerUrl,
+    [string]$DeviceId = [System.Guid]::NewGuid().ToString(),
+    [string]$DeviceSecret = ""
+)
+
 $ErrorActionPreference = 'Stop'
 $adb = if ($env:LOCALAPPDATA) { "$env:LOCALAPPDATA\Android\Sdk\platform-tools\adb.exe" } else { "adb" }
 
-$xml = @'
+$xml = @"
 <?xml version='1.0' encoding='utf-8' standalone='yes' ?>
 <map>
-    <string name="flutter.peerm_device_secret">138e882888efa3a8d489225ecc75aa945974c7f7b225414a</string>
-    <string name="flutter.peerm_device_id">bdc33e9b-8b63-45ae-9dd8-2c37aa1d7b36</string>
-    <string name="flutter.peerm_server_url">ws://10.84.188.119:8080</string>
+    <string name="flutter.peerm_device_secret">$DeviceSecret</string>
+    <string name="flutter.peerm_device_id">$DeviceId</string>
+    <string name="flutter.peerm_server_url">$ServerUrl</string>
 </map>
-'@
+"@
 
 $b64 = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($xml))
 & $adb shell "run-as com.peerm.peerm_app sh -c 'echo $b64 | base64 -d > shared_prefs/FlutterSharedPreferences.xml'"
